2f2b35d5b7b032b70cfcd584363f4a462d7d0a19
Vitaly Spirin <vitaly.spirin@gmail.com>
2025-04-12 20:56:59 -0400

    Create verbsInConditionalPresentTense.mjs

---
+++ french/verbsInConditionalPresentTense.mjs
@@ -0,0 +1,731 @@
+// Author: Vitaly Spirin
+//
+// verb list is taken from 
+// https://oraprdnt.uqtr.uquebec.ca/portail/docs/GSC2213/F851010166_Le_conditionnel_pr_sent_exercices_et_corrig_.pdf
+
+"use strict";
+
+export const audioFileFolder = 'conditionalpresent';
+
+export default {
+    Avoir: {
+        "J'": "aurais",
+        Tu: "aurais",
+        Il: "aurait",
+        Nous: "aurions",
+        Vous: "auriez",
+        Ils: "auraient"
+    },
+    Être: {
+        Je: "serais",
+        Tu: "serais",
+        Il: "serait",
+        Nous: "serions",
+        Vous: "seriez",
+        Ils: "seraient"
+    },
+    Aller: {
+        "J'": "irais",
+        Tu: "irais",
+        Il: "irait",
+        Nous: "irions",
+        Vous: "iriez",
+        Ils: "iraient"
+    },
+    Venir: {
+        Je: "viendrais",
+        Tu: "viendrais",
+        Il: "viendrait",
+        Nous: "viendrions",
+        Vous: "viendriez",
+        Ils: "viendraient"
+    },
+    Faire: {
+        Je: "ferais",
+        Tu: "ferais",
+        Il: "ferait",
+        Nous: "ferions",
+        Vous: "feriez",
+        Ils: "feraient"
+    },
+    Savoir: {
+        Je: "saurais",
+        Tu: "saurais",
+        Il: "saurait",
+        Nous: "saurions",
+        Vous: "sauriez",
+        Ils: "sauraient"
+    },
+    Pouvoir: {
+        Je: "pourrais",
+        Tu: "pourrais",
+        Il: "pourrait",
+        Nous: "pourrions",
+        Vous: "pourriez",
+        Ils: "pourraient"
+    },
+    Devoir: {
+        Je: "devrais",
+        Tu: "devrais",
+        Il: "devrait",
+        Nous: "devrions",
+        Vous: "devriez",
+        Ils: "devraient"
+    },
+    Vouloir: {
+        Je: "voudrais",
+        Tu: "voudrais",
+        Il: "voudrait",
+        Nous: "voudrions",
+        Vous: "voudriez",
+        Ils: "voudraient"
+    },
+    Aimer: {
+        "J'": "aimerais",
+        Tu: "aimerais",
+        Il: "aimerait",
+        Nous: "aimerions",
+        Vous: "aimeriez",
+        Ils: "aimeraient"
+    },
+    Parler: {
+        Je: "parlerais",
+        Tu: "parlerais",
+        Il: "parlerait",
+        Nous: "parlerions",
+        Vous: "parleriez",
+        Ils: "parleraient"
+    },
+    Appeler: {
+        "J'": "appellerais",
+        Tu: "appellerais",
+        Il: "appellerait",
+        Nous: "appellerions",
+        Vous: "appelleriez",
+        Ils: "appelleraient"
+    },
+    "S'ennuyer": {
+        Je: "m'ennuierais",
+        Tu: "t'ennuierais",
+        Il: "s'ennuierait",
+        Nous: "nous ennuierions",
+        Vous: "vous ennuieriez",
+        Ils: "s'ennuieraient"
+    },
+    Employer: {
+        "J'": "emploierais",
+        Tu: "emploierais",
+        Il: "emploierait",
+        Nous: "emploierions",
+        Vous: "emploieriez",
+        Ils: "emploieraient"
+    },
+    Jeter: {
+        Je: "jetterais",
+        Tu: "jetterais",
+        Il: "jetterait",
+        Nous: "jetterions",
+        Vous: "jetteriez",
+        Ils: "jetteraient"
+    },
+    Payer: {
+        Je: "payerais",
+        Tu: "payerais",
+        Il: "payerait",
+        Nous: "payerions",
+        Vous: "payeriez",
+        Ils: "payeraient"
+    },
+    Envoyer: {
+        "J'": "enverrais",
+        Tu: "enverrais",
+        Il: "enverrait",
+        Nous: "enverrions",
+        Vous: "enverriez",
+        Ils: "enverraient"
+    },
+    Acheter: {
+        "J'": "achèterais",
+        Tu: "achèterais",
+        Il: "achèterait",
+        Nous: "achèterions",
+        Vous: "achèteriez",
+        Ils: "achèteraient"
+    },
+    "Se promener": {
+        "J'": "me promènerais",
+        Tu: "te promènerais",
+        Il: "se promènerait",
+        Nous: "nous promènerions",
+        Vous: "vous promèneriez",
+        Ils: "se promèneraient"
+    },
+    "Se lever": {
+        "J'": "me lèverais",
+        Tu: "te lèverais",
+        Il: "se lèverait",
+        Nous: "nous lèverions",
+        Vous: "vous lèveriez",
+        Ils: "se lèveraient"
+    },
+    Soulever: {
+        Je: "soulèverais",
+        Tu: "soulèverais",
+        Il: "soulèverait",
+        Nous: "soulèverions",
+        Vous: "soulèveriez",
+        Ils: "soulèveraient"
+    },
+    Achever: {
+        "J'": "achèverais",
+        Tu: "achèverais",
+        Il: "achèverait",
+        Nous: "achèverions",
+        Vous: "achèveriez",
+        Ils: "achèveraient"
+    },
+    Geler: {
+        Je: "gèlerai",
+        Tu: "gèleras",
+        Il: "gèlera",
+        Nous: "gèlerons",
+        Vous: "gèlerez",
+        Ils: "gèleront"
+    },
+    Congeler: {
+        Je: "congèlerais",
+        Tu: "congèlerais",
+        Il: "congèlerait",
+        Nous: "congèlerions",
+        Vous: "congèleriez",
+        Ils: "congèleraient"
+    },
+    Modeler: {
+        Je: "modèlerai",
+        Tu: "modèleras",
+        Il: "modèlera",
+        Nous: "modèlerons",
+        Vous: "modèlerez",
+        Ils: "modèleront"
+    },
+    Harceler: {
+        "J'": "harcèlerais",
+        Tu: "harcèlerais",
+        Il: "harcèlerait",
+        Nous: "harcèlerions",
+        Vous: "harcèleriez",
+        Ils: "harcèleraient"
+    },
+    Peler: {
+        Je: "pèlerais",
+        Tu: "pèlerais",
+        Il: "pèlerait",
+        Nous: "pèlerions",
+        Vous: "pèleriez",
+        Ils: "pèleraient"
+    },
+    Enlever: {
+        "J'": "enlèverai",
+        Tu: "enlèveras",
+        Il: "enlèvera",
+        Nous: "enlèverons",
+        Vous: "enlèverez",
+        Ils: "enlèveront"
+    },
+    Amener: {
+        "J'": "amènerai",
+        Tu: "amèneras",
+        Il: "amènera",
+        Nous: "amènerons",
+        Vous: "amènerez",
+        Ils: "amèneront"
+    },
+    Emmener: {
+        "J'": "emmènerai",
+        Tu: "emmèneras",
+        Il: "emmènera",
+        Nous: "emmènerons",
+        Vous: "emmènerez",
+        Ils: "emmèneront"
+    },
+    Mener: {
+        Je: "mènerai",
+        Tu: "mèneras",
+        Il: "mènera",
+        Nous: "mènerons",
+        Vous: "mènerez",
+        Ils: "mèneront"
+    },
+    Semer: {
+        Je: "sèmerai",
+        Tu: "sèmeras",
+        Il: "sèmera",
+        Nous: "sèmerons",
+        Vous: "sèmerez",
+        Ils: "sèmeront"
+    },
+    Peser: {
+        Je: "pèserai",
+        Tu: "pèseras",
+        Il: "pèsera",
+        Nous: "pèserons",
+        Vous: "pèserez",
+        Ils: "pèseront"
+    },
+    Partir: {
+        Je: "partirais",
+        Tu: "partirais",
+        Il: "partirait",
+        Nous: "partirions",
+        Vous: "partiriez",
+        Ils: "partiraient"
+    },
+    Finir: {
+        Je: "finirais",
+        Tu: "finirais",
+        Il: "finirait",
+        Nous: "finirions",
+        Vous: "finiriez",
+        Ils: "finiraient"
+    },
+    Haïr: {
+        "J'": "haïrais",
+        Tu: "haïrais",
+        Il: "haïrait",
+        Nous: "haïrions",
+        Vous: "haïriez",
+        Ils: "haïraient"
+    },
+    Dormir: {
+        Je: "dormirais",
+        Tu: "dormirais",
+        Il: "dormirait",
+        Nous: "dormirions",
+        Vous: "dormiriez",
+        Ils: "dormiraient"
+    },
+    Fuir: {
+        Je: "fuirais",
+        Tu: "fuirais",
+        Il: "fuirait",
+        Nous: "fuirions",
+        Vous: "fuiriez",
+        Ils: "fuiraient"
+    },
+    Offrir: {
+        "J'": "offrirais",
+        Tu: "offrirais",
+        Il: "offrirait",
+        Nous: "offririons",
+        Vous: "offririez",
+        Ils: "offriraient"
+    },
+    Ouvrir: {
+        "J'": "ouvrirais",
+        Tu: "ouvrirais",
+        Il: "ouvrirait",
+        Nous: "ouvririons",
+        Vous: "ouvririez",
+        Ils: "ouvriraient"
+    },
+    Ouvrir: {
+        Je: "couvrirais",
+        Tu: "couvrirais",
+        Il: "couvrirait",
+        Nous: "couvririons",
+        Vous: "couvririez",
+        Ils: "couvriraient"
+    },
+    Découvrir: {
+        Je: "découvrirais",
+        Tu: "découvrirais",
+        Il: "découvrirait",
+        Nous: "découvririons",
+        Vous: "découvririez",
+        Ils: "découvriraient"
+    },
+    Souffrir: {
+        Je: "souffrirais",
+        Tu: "souffrirais",
+        Il: "souffrirait",
+        Nous: "souffririons",
+        Vous: "souffririez",
+        Ils: "souffriraient"
+    },
+    "Se servir": {
+        Je: "me servirais",
+        Tu: "te servirais",
+        Il: "se servirait",
+        Nous: "nous servirions",
+        Vous: "vous serviriez",
+        Ils: "se serviraient"
+    },
+    "S'enrichir": {
+        Je: "m'enrichirais",
+        Tu: "t'enrichirais",
+        Il: "s'enrichirait",
+        Nous: "nous enrichirions",
+        Vous: "vous enrichiriez",
+        Ils: "s'enrichiraient"
+    },
+    "S'évanouir": {
+        Je: "m'évanouirais",
+        Tu: "t'évanouirais",
+        Il: "s'évanouirait",
+        Nous: "nous évanouirions",
+        Vous: "vous évanouiriez",
+        Ils: "s'évanouiraient"
+    },
+    Mourir: {
+        Je: "mourrais",
+        Tu: "mourrais",
+        Il: "mourrait",
+        Nous: "mourrions",
+        Vous: "mourriez",
+        Ils: "mourraient"
+    },
+    Courir: {
+        Je: "courrais",
+        Tu: "courrais",
+        Il: "courrait",
+        Nous: "courrions",
+        Vous: "courriez",
+        Ils: "courraient"
+    },
+    Acquérir: {
+        "J'": "acquerrais",
+        Tu: "acquerrais",
+        Il: "acquerrait",
+        Nous: "acquerrions",
+        Vous: "acquerriez",
+        Ils: "acquerraient"
+    },
+    Tenir: {
+        Je: "tiendrais",
+        Tu: "tiendrais",
+        Il: "tiendrait",
+        Nous: "tiendrions",
+        Vous: "tiendriez",
+        Ils: "tiendraient"
+    },
+    Venir: {
+        Je: "viendrais",
+        Tu: "viendrais",
+        Il: "viendrait",
+        Nous: "viendrions",
+        Vous: "viendriez",
+        Ils: "viendraient"
+    },
+    Attendre: {
+        "J'": "attendrais",
+        Tu: "attendrais",
+        Il: "attendrait",
+        Nous: "attendrions",
+        Vous: "attendriez",
+        Ils: "attendraient"
+    },
+    Prendre: {
+        Je: "prendrais",
+        Tu: "prendrais",
+        Il: "prendrait",
+        Nous: "prendrions",
+        Vous: "prendriez",
+        Ils: "prendraient"
+    },
+    Répondre: {
+        Je: "répondrais",
+        Tu: "répondrais",
+        Il: "répondrait",
+        Nous: "répondrions",
+        Vous: "répondriez",
+        Ils: "répondraient"
+    },
+    Lire: {
+        Je: "lirais",
+        Tu: "lirais",
+        Il: "lirait",
+        Nous: "lirions",
+        Vous: "liriez",
+        Ils: "liraient"
+    },
+    Conduire: {
+        Je: "conduirais",
+        Tu: "conduirais",
+        Il: "conduirait",
+        Nous: "conduirions",
+        Vous: "conduiriez",
+        Ils: "conduiraient"
+    },
+    Dire: {
+        Je: "dirais",
+        Tu: "dirais",
+        Il: "dirait",
+        Nous: "dirions",
+        Vous: "diriez",
+        Ils: "diraient"
+    },
+    Rire: {
+        Je: "rirais",
+        Tu: "rirais",
+        Il: "rirait",
+        Nous: "ririons",
+        Vous: "ririez",
+        Ils: "riraient"
+    },
+    Écrire: {
+        "J'": "écrirais",
+        Tu: "écrirais",
+        Il: "écrirait",
+        Nous: "écririons",
+        Vous: "écririez",
+        Ils: "écriraient"
+    },
+    Perdre: {
+        Je: "perdrais",
+        Tu: "perdrais",
+        Il: "perdrait",
+        Nous: "perdrions",
+        Vous: "perdriez",
+        Ils: "perdraient"
+    },
+    Peindre: {
+        Je: "peindrais",
+        Tu: "peindrais",
+        Il: "peindrait",
+        Nous: "peindrions",
+        Vous: "peindriez",
+        Ils: "peindraient"
+    },
+    "Se plaindre": {
+        Je: "me plaindrais",
+        Tu: "te plaindrais",
+        Il: "se plaindrait",
+        Nous: "nous plaindrions",
+        Vous: "vous plaindriez",
+        Ils: "se plaindraient"
+    },
+    Coudre: {
+        Je: "coudrais",
+        Tu: "coudrais",
+        Il: "coudrait",
+        Nous: "coudrions",
+        Vous: "coudriez",
+        Ils: "coudraient"
+    },
+    Résoudre: {
+        Je: "résoudrais",
+        Tu: "résoudrais",
+        Il: "résoudrait",
+        Nous: "résoudrions",
+        Vous: "résoudriez",
+        Ils: "résoudraient"
+    },
+    Rejoindre: {
+        Je: "rejoindrais",
+        Tu: "rejoindrais",
+        Il: "rejoindrait",
+        Nous: "rejoindrions",
+        Vous: "rejoindriez",
+        Ils: "rejoindraient"
+    },
+    Plaindre: {
+        Je: "plaindrais",
+        Tu: "plaindrais",
+        Il: "plaindrait",
+        Nous: "plaindrions",
+        Vous: "plaindriez",
+        Ils: "plaindraient"
+    },
+    Boire: {
+        Je: "boirais",
+        Tu: "boirais",
+        Il: "boirait",
+        Nous: "boirions",
+        Vous: "boiriez",
+        Ils: "boiraient"
+    },
+    Croire: {
+        Je: "croirais",
+        Tu: "croirais",
+        Il: "croirait",
+        Nous: "croirions",
+        Vous: "croiriez",
+        Ils: "croiraient"
+    },
+    Interrompre: {
+        "J'": "interromprais",
+        Tu: "interromprais",
+        Il: "interromprait",
+        Nous: "interromprions",
+        Vous: "interrompriez",
+        Ils: "interrompraient"
+    },
+    Distraire: {
+        Je: "distrairais",
+        Tu: "distrairais",
+        Il: "distrairait",
+        Nous: "distrairions",
+        Vous: "distrairiez",
+        Ils: "distrairaient"
+    },
+    Mettre: {
+        Je: "mettrais",
+        Tu: "mettrais",
+        Il: "mettrait",
+        Nous: "mettrions",
+        Vous: "mettriez",
+        Ils: "mettraient"
+    },
+    Connaître: {
+        Je: "connaîtrais",
+        Tu: "connaîtrais",
+        Il: "connaîtrait",
+        Nous: "connaîtrions",
+        Vous: "connaîtriez",
+        Ils: "connaîtraient"
+    },
+    Convaincre: {
+        Je: "convaincrais",
+        Tu: "convaincrais",
+        Il: "convaincrait",
+        Nous: "convaincrions",
+        Vous: "convaincriez",
+        Ils: "convaincraient"
+    },
+    Battre: {
+        Je: "battrais",
+        Tu: "battrais",
+        Il: "battrait",
+        Nous: "battrions",
+        Vous: "battriez",
+        Ils: "battraient"
+    },
+    Naître: {
+        Je: "naîtrais",
+        Tu: "naîtrais",
+        Il: "naîtrait",
+        Nous: "naîtrions",
+        Vous: "naîtriez",
+        Ils: "naîtraient"
+    },
+    "Se plaire": {
+        Je: "me plairais",
+        Tu: "te plairais",
+        Il: "se plairait",
+        Nous: "nous plairions",
+        Vous: "vous plairiez",
+        Ils: "se plairaient"
+    },
+    Déplaire: {
+        Je: "déplairais",
+        Tu: "déplairais",
+        Il: "déplairait",
+        Nous: "déplairions",
+        Vous: "déplairiez",
+        Ils: "déplairaient"
+    },
+    Naître: {
+        Je: "naîtrais",
+        Tu: "naîtrais",
+        Il: "naîtrait",
+        Nous: "naîtrions",
+        Vous: "naîtriez",
+        Ils: "naîtraient"
+    },
+    Vivre: {
+        Je: "vivrais",
+        Tu: "vivrais",
+        Il: "vivrait",
+        Nous: "vivrions",
+        Vous: "vivriez",
+        Ils: "vivraient"
+    },
+    Suivre: {
+        Je: "suivrais",
+        Tu: "suivrais",
+        Il: "suivrait",
+        Nous: "suivrions",
+        Vous: "suivriez",
+        Ils: "suivraient"
+    },
+    Conclure: {
+        Je: "conclurais",
+        Tu: "conclurais",
+        Il: "conclurait",
+        Nous: "conclurions",
+        Vous: "concluriez",
+        Ils: "concluraient"
+    },
+    "Se taire": {
+        Je: "me tairais",
+        Tu: "te tairais",
+        Il: "se tairait",
+        Nous: "nous tairions",
+        Vous: "vous tairiez",
+        Ils: "se tairaient"
+    },
+    Voir: {
+        Je: "verrais",
+        Tu: "verrais",
+        Il: "verrait",
+        Nous: "verrions",
+        Vous: "verriez",
+        Ils: "verraient"
+    },
+    Valoir: {
+        Je: "vaudrais",
+        Tu: "vaudrais",
+        Il: "vaudrait",
+        Nous: "vaudrions",
+        Vous: "vaudriez",
+        Ils: "vaudraient"
+    },
+    "S'émouvoir": {
+        Je: "m'émouvrais",
+        Tu: "t'émouvrais",
+        Il: "s'émouvrait",
+        Nous: "nous émouvrions",
+        Vous: "vous émouvriez",
+        Ils: "s'émouvraient"
+    },
+    Recevoir: {
+        Je: "recevrais",
+        Tu: "recevrais",
+        Il: "recevrait",
+        Nous: "recevrions",
+        Vous: "recevriez",
+        Ils: "recevraient"
+    },
+    Décevoir: {
+        Je: "décevrais",
+        Tu: "décevrais",
+        Il: "décevrait",
+        Nous: "décevrions",
+        Vous: "décevriez",
+        Ils: "décevraient"
+    },
+    "S'apercevoir": {
+        Je: "m'apercevrais",
+        Tu: "t'apercevrais",
+        Il: "s'apercevrait",
+        Nous: "nous apercevrions",
+        Vous: "vous apercevriez",
+        Ils: "s'apercevraient"
+    },
+    "S'asseoir": {
+        Je: "m'assiérais",
+        Tu: "t'assiérais",
+        Il: "s'assiérait",
+        Nous: "nous assiérions",
+        Vous: "vous assiériez",
+        Ils: "s'assiéraient"
+    },
+    Falloir: {
+        Il: "faudrait"
+    },
+    Pleuvoir: {
+        Il: "pleuvrait"
+    }
+
+
+};
