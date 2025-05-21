2ca7bef6e159ce34d3e7c22a75e4cb2301cfa191
Vitaly Spirin <vitaly.spirin@gmail.com>
2025-05-21 20:54:15 +0000

    added saving Error Counter into localStorage

--- french/js/ErrorCounter.mjs
+++ french/js/ErrorCounter.mjs
@@ -3,12 +3,21 @@
 "use strict";
 
 export class ErrorCounter {
+    static timestamp;
+    static numberOfCompleted = 0;
+    static numberOfAllInputElements;
+
     static initialize() {
         const inputElementList = document.getElementsByTagName('input');
 
         for (const inputElement of inputElementList) {
             inputElement.addEventListener("focusout", this.focusOutEventHandler);
         }
+
+        this.numberOfAllInputElements = inputElementList.length;
+        this.timestamp = Date.now();
+
+        this.showStats();
     }
 
     static focusOutEventHandler(event) {
@@ -19,10 +28,59 @@ export class ErrorCounter {
             let errorCounter = Number(errorCounterElement.innerText);
             errorCounter++;
             errorCounterElement.textContent = String(errorCounter);
-
             event.target.removeEventListener("focusout", ErrorCounter.focusOutEventHandler);
+
+            if (this.numberOfCompleted > this.numberOfAllInputElements / 2) {
+                ErrorCounter.saveCounter(errorCounter);
+            }
+        } else if (event.target.value != '') {
+            this.numberOfCompleted++;
         }
 
     }
 
+    static saveCounter(errorCounter) {
+        let stats = this.retrieveStats();
+        stats[this.timestamp] = errorCounter;
+        localStorage.setItem(this.getStorageKey(), JSON.stringify(stats));
+    }
+
+    static retrieveStats() {
+        let stats;
+
+        const storageValue = localStorage.getItem(this.getStorageKey());
+        if (storageValue == null) {
+            stats = {};
+        } else {
+            stats = JSON.parse(storageValue);
+        }
+
+        return stats;
+    }
+
+    static showStats() {
+        const template = document.getElementById('template-stats-line')
+
+        if (!(template instanceof HTMLTemplateElement)) return;
+        const statsLineElement = template.content.firstElementChild;
+
+        const stats = this.retrieveStats();
+        const keys = Object.keys(stats).reverse();
+
+        keys.forEach( (key) => {
+            let newStateLineElement = statsLineElement.cloneNode();
+            document.getElementById('stats').appendChild(newStateLineElement);
+            let theDate = new Date(Number(key));
+
+            let formattedDate = theDate.getFullYear() + '-' + theDate.getMonth() + '-' +
+                theDate.getDate() + ' ' + theDate.getHours() + ':' + theDate.getMinutes();
+
+            newStateLineElement.textContent = formattedDate + ': ' + stats[key];
+
+        });
+    }
+
+    static getStorageKey() {
+        return window.location.pathname.split('/').pop() + window.location.search;
+    }
 }
