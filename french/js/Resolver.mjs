1926d9d23e7b2422517a57498d46a6d7c25fd7c6
Vitaly Spirin <vitaly.spirin@gmail.com>
2026-03-12 13:20:19 -0400

    moved conjugations.html into subfolder conjugations

--- french/js/Resolver.mjs
+++ french/js/Resolver.mjs
@@ -65,73 +65,69 @@ export class Resolver {
 
 
     static infoForPages = {
-        "conjugations.html?Le%20pr%C3%A9sent": "pdf/present.pdf",
+        // A1
+        "exercise_noun_number.html": "../pdf/noun_gender_number.pdf",
+        "conjugations.html?Le%20pr%C3%A9sent": "../pdf/present.pdf",
         "exercise_present.html": "../pdf/present.pdf",
-
         "exercise_devoir_pouvoir_falloir_vouloir.html": "../../pdf/devoir_pouvoir_falloir_vouloir.pdf",
-
+        "exercise_meteo.html": "../pdf/meteo.pdf",
+        "exercise_calendar.html": "../pdf/calendar.pdf",
         "exercise_mon_ton_son.html": "pdf/mon_ton_son.pdf",
-        "exercise_mien_sien.html": "pdf/mon_ton_son.pdf",
         "exercise_de_du_des.html": "pdf/de_du_des.pdf",
         "exercise_ce_ces_cet.html": "pdf/ce_ces_cet.pdf",
-
+        "exercise_quand_comment_combien.html": "pdf/quand_comment_combien.pdf",
+        "conjugations.html?Le%20pass%C3%A9%20compos%C3%A9": "../pdf/presentperfect.pdf",
+        "exercise_presentperfect.html": "../pdf/presentperfect.pdf",
         "exercise_qui_quel_quelle.html": "pdf/qui_quel_quelle.pdf",
 
-        "exercise_quand_comment_combien.html": "pdf/quand_comment_combien.pdf",
-
-        "conjugations.html?Le%20pass%C3%A9%20compos%C3%A9": "pdf/presentperfect.pdf",
-        "exercise_presentperfect.html": "../pdf/presentperfect.pdf",
-
-        "conjugations.html?L%27imparfait": "pdf/imperfect.pdf",
-        "exercise_imperfect.html": "../pdf/imperfect.pdf",
-
-        "conjugations.html?Le%20futur%20simple": "pdf/future.pdf",
-        "exercise_future.html": "../pdf/future.pdf",
-
-        "conjugations.html?L%27imp%C3%A9ratif": "pdf/imperative.pdf",
-        "exercise_imperative.html": "../pdf/imperative.pdf",
-
-        "exercise_avoir_etre.html": "../../pdf/avoir_etre.pdf",
-        "conjugations.html?Le%20plus-que-parfait": "pdf/plus-que-parfait.pdf",
-        "exercise_pastperfect.html": "../pdf/plus-que-parfait.pdf",
-
-        "conjugations.html?Le%20conditionnel%20pr%C3%A9sent": "pdf/conditionalpresent.pdf",
-        "exercise_conditionalpresent.html": "../pdf/conditionalpresent.pdf",
-
-        "conjugations.html?Le%20subjonctif%20pr%C3%A9sent": "pdf/presentsubjunctive.pdf",
-        "exercise_presentsubjunctive.html": "../pdf/presentsubjunctive.pdf",
-        "conjugations.html?Le%20subjonctif%20pass%C3%A9": "pdf/presentsubjunctive.pdf",
-        "exercise_presentperfectsubjunctive.html": "../pdf/presentsubjunctive.pdf",
-
-        "conjugations.html?Le%20conditionnel%20pass%C3%A9": "pdf/conditionalperfect.pdf",
-        "exercise_conditionalperfect.html": "../pdf/conditionalperfect.pdf",
-
-        "conjugations.html?Le%20futur%20ant%C3%A9rieur": "pdf/futureperfect.pdf",
-        "exercise_futureperfect.html": "../pdf/futureperfect.pdf",
-
-        "exercise_concordance.html": "../pdf/concordance2.pdf",
-        "exercise_concordance3.html": "../pdf/concordance3.pdf",
-        "exercise_passive.html": "../../pdf/passive.pdf",
-        "exercise_concordance_conditional.html": "../pdf/concordance_conditional.pdf",
-        "exercise_concordance_future.html": "../pdf/concordance_future.pdf",
-
+        // A2
         "exercise_noun_gender.html": "../pdf/noun_gender_number.pdf",
-        "exercise_noun_number.html": "../pdf/noun_gender_number.pdf",
+        "exercise_avoir_etre.html": "../../pdf/avoir_etre.pdf",
         "exercise_adjective_position.html": "../pdf/adjective_position.pdf",
         "exercise_adjective_gender.html": "../pdf/adjective_gender_number.pdf",
         "exercise_adjective_number.html": "../pdf/adjective_gender_number.pdf",
         "exercise_il_est.html": "../pdf/il_est.pdf",
+        "exercise_mien_sien.html": "pdf/mon_ton_son.pdf",
+        "conjugations.html?L%27imparfait": "../pdf/imperfect.pdf",
+        "exercise_imperfect.html": "../pdf/imperfect.pdf",
+        "exercise_concordance.html": "../pdf/concordance2.pdf",
+        "conjugations.html?Le%20futur%20simple": "../pdf/future.pdf",
+        "exercise_future.html": "../pdf/future.pdf",
         "exercise_time_prepositions.html": "pdf/time_prepositions.pdf",
-        "exercise_y_en.html": "pdf/y_en.pdf",
         "exercise_qui_que.html": "pdf/qui_que.pdf",
-        "exercise_ceci_cela_celui.html": "pdf/ce_ces_cet.pdf",
         "exercise_lui_leur.html": "pdf/lui_leur.pdf",
         "exercise_negation.html": "pdf/negation.pdf",
+        "conjugations.html?L%27imp%C3%A9ratif": "../pdf/imperative.pdf",
+        "exercise_imperative.html": "../pdf/imperative.pdf",
+
+        // B1
+        "exercise_negation2.html": "pdf/negation.pdf",
+        "exercise_ceci_cela_celui.html": "pdf/ce_ces_cet.pdf",
+        "exercise_y_en.html": "pdf/y_en.pdf",
         "exercise_tout.html": "../pdf/tout.pdf",
         "exercise_que_dont.html": "pdf/que_dont.pdf",
         "exercise_qui_que_dont_ou.html": "pdf/qui_que_dont_ou.pdf",
+        "conjugations.html?Le%20plus-que-parfait": "../pdf/plus-que-parfait.pdf",
+        "exercise_pastperfect.html": "../pdf/plus-que-parfait.pdf",
+        "exercise_concordance3.html": "../pdf/concordance3.pdf",
+        "conjugations.html?Le%20conditionnel%20pr%C3%A9sent": "../pdf/conditionalpresent.pdf",
+        "exercise_conditionalpresent.html": "../pdf/conditionalpresent.pdf",
         "exercise_savoir_connaitre.html": "../pdf/savoir_connaitre.pdf",
-        "exercise_amener_emmener.html": "../pdf/amener_emmener.pdf"
+        "exercise_amener_emmener.html": "../pdf/amener_emmener.pdf",
+        "conjugations.html?Le%20subjonctif%20pr%C3%A9sent": "../pdf/presentsubjunctive.pdf",
+        "exercise_presentsubjunctive.html": "../pdf/presentsubjunctive.pdf",
+
+        // B2
+        "conjugations.html?Le%20subjonctif%20pass%C3%A9": "../pdf/presentsubjunctive.pdf",
+        "exercise_presentperfectsubjunctive.html": "../pdf/presentsubjunctive.pdf",
+        "conjugations.html?Le%20conditionnel%20pass%C3%A9": "../pdf/conditionalperfect.pdf",
+        "exercise_conditionalperfect.html": "../pdf/conditionalperfect.pdf",
+        "exercise_concordance_conditional.html": "../pdf/concordance_conditional.pdf",
+        "exercise_passive.html": "../../pdf/passive.pdf",
+        "conjugations.html?Le%20futur%20ant%C3%A9rieur": "../pdf/futureperfect.pdf",
+        "exercise_futureperfect.html": "../pdf/futureperfect.pdf",
+        "exercise_concordance_future.html": "../pdf/concordance_future.pdf",
+
     };
 
 
