91731f050894d759652faee6c3294e2349050b4a
Vitaly Spirin <vitaly.spirin@gmail.com>
2026-05-07 17:57:17 -0400

    added text exercise for Lequel-duquel-auquel

--- french/js/Resolver.mjs
+++ french/js/Resolver.mjs
@@ -124,6 +124,7 @@ export class Resolver {
         "exercise-noun-gender-3.html": "../pdf/noun-gender-2.pdf",
         "exercise_ce_qui_ce_que.html": "../pdf/ce-qui-ce-que.pdf",
         "exercise_dont_duquel.html": "../pdf/dont_duquel.pdf",
+        "exercise_lequel_duquel_auquel.html": "../pdf/lequel_duquel_auquel.pdf",
         "conjugations.html?Le%20subjonctif%20pass%C3%A9": "../pdf/present-subjunctive.pdf",
         "exercise_presentperfectsubjunctive.html": "../pdf/present-subjunctive.pdf",
         "conjugations.html?Le%20conditionnel%20pass%C3%A9": "../pdf/conditional-perfect.pdf",
