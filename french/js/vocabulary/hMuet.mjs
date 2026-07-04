aa60ac862d11af70a0ac28fa6f0a57f24878dbdc
Vitaly Spirin <vitaly.spirin@gmail.com>
2026-07-03 20:23:04 -0400

    added hMuet.mjs to js/vocabulary folder

---
+++ french/js/vocabulary/hMuet.mjs
@@ -0,0 +1,17 @@
+// Author: Vitaly Spirin
+//
+// word list is taken from 
+// https://oraprdnt.uqtr.uquebec.ca/portail/docs/GSC2213/F507117213_Le_H_muet_ou_aspir__perf._exercices_et_corrig__web.pdf
+// https://oraprdnt.uqtr.uquebec.ca/portail/docs/GSC2213/F101668740_Le_H_muet_ou_aspir__exercices_et_corrig__web.pdf
+//
+
+"use strict";
+
+export const audioFileFolder = 'hmuet';
+
+export default {
+    "habileté": {
+        article: "l'",
+        level: "B2"
+    }
+}
