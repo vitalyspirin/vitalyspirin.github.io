906cbc5778caaa27ea6fe40d63fee46f843c9a61
Vitaly Spirin <vitaly.spirin@gmail.com>
2026-07-07 18:57:36 -0400

    refactored h-muet to use js to build a page from json list of words

---
+++ french/js/PageBuilderForWordList.mjs
@@ -0,0 +1,45 @@
+// Author: Vitaly Spirin
+
+"use strict";
+
+import { Resolver } from './Resolver.mjs';
+import Types from './Types.mjs';
+import Utils from './Utils.mjs';
+
+export default class PageBuilderForWordList {
+
+    /**
+     * @param {Record<string, {article: string, level: string}>} wordListStructure 
+     */
+    static build(wordListStructure) {
+        const wordList = document.getElementById('word-list');
+
+        let templateBlock = Types.assertType(
+            document.getElementById('template-li-for-one-word'),
+            HTMLTemplateElement
+        ).content.firstElementChild;
+
+        for (let word in wordListStructure) {
+            let worldLiBlock = templateBlock.cloneNode(true);
+
+            worldLiBlock.classList.add(wordListStructure[word].level);
+            
+            let wordElement = worldLiBlock.querySelector('[title="article"]');
+            wordElement.textContent = word;
+            wordElement.setAttribute('title', wordListStructure[word].article);
+
+            worldLiBlock.querySelectorAll('input')
+            .forEach((/** @type {HTMLInputElement} */ inputElement) => {
+                inputElement.setAttribute('name', word);
+                if (inputElement.value === wordListStructure[word].article) {
+                    inputElement.setAttribute('required', 'required');
+                }
+            });
+
+            wordList.append(worldLiBlock);
+        };
+
+
+    }
+
+}
--- french/js/vocabulary/hMuet.mjs
+++ french/js/vocabulary/hMuet.mjs
@@ -7,10 +7,378 @@
 
 "use strict";
 
-export const audioFileFolder = 'hmuet';
+export const audioFileFolder = 'vocabulary/hmuet';
 
 export default {
     "habileté": {
+        article: "l'",
+        level: "B2"
+    },
+    "handicap": {
+        article: "le",
+        level: "B1"
+    },
+    "hoquet": {
+        article: "le",
+        level: "B1"
+    },
+    "harfang": {
+        article: "le",
+        level: "C1"
+    },
+    "herpès": {
+        article: "l'",
+        level: "B2"
+    },
+    "hardiesse": {
+        article: "la",
+        level: "C1"
+    },
+    "horaire": {
+        article: "l'",
+        level: "A2"
+    },
+    "humeur": {
+        article: "l'",
+        level: "A2"
+    },
+    "hutte": {
+        article: "la",
+        level: "B1"
+    },
+    "haut-parleur": {
+        article: "le",
+        level: "A2"
+    },
+    "haltère": {
+        article: "l'",
+        level: "B1"
+    },
+    "héron": {
+        article: "le",
+        level: "B2"
+    },
+    "hibou": {
+        article: "le",
+        level: "A2"
+    },
+    "hérédité": {
+        article: "l'",
+        level: "B2"
+    },
+    "hotte": {
+        article: "la",
+        level: "B1"
+    },
+    "hochet": {
+        article: "le",
+        level: "B2"
+    },
+    "homéopathie": {
+        article: "l'",
+        level: "B2"
+    },
+    "hangar": {
+        article: "le",
+        level: "B1"
+    },
+    "hareng": {
+        article: "le",
+        level: "B1"
+    },
+    "haleine": {
+        article: "l'",
+        level: "B1"
+    },
+    "hospice": {
+        article: "l'",
+        level: "B2"
+    },
+    "houblon": {
+        article: "le",
+        level: "B2"
+    },
+    "harpe": {
+        article: "la",
+        level: "A2"
+    },
+    "habitat": {
+        article: "l'",
+        level: "B1"
+    },
+    "héros": {
+        article: "le",
+        level: "A2"
+    },
+    "hache": {
+        article: "la",
+        level: "A2"
+    },
+    "horoscope": {
+        article: "l'",
+        level: "A2"
+    },
+    "humour": {
+        article: "l'",
+        level: "A2"
+    },
+    "haine": {
+        article: "la",
+        level: "B1"
+    },
+    "huile": {
+        article: "l'",
+        level: "A1"
+    },
+    "hamburger": {
+        article: "le",
+        level: "A1"
+    },
+    "hebdomadaire": {
+        article: "l'",
+        level: "B1"
+    },
+    "haie": {
+        article: "la",
+        level: "A2"
+    },
+    "homicide": {
+        article: "l'",
+        level: "B2"
+    },
+    "hippodrome": {
+        article: "l'",
+        level: "B1"
+    },
+    "hockey": {
+        article: "le",
+        level: "A1"
+    },
+    "hausse": {
+        article: "la",
+        level: "B1"
+    },
+    "hanche": {
+        article: "la",
+        level: "A2"
+    },
+    "herbivore": {
+        article: "l'",
+        level: "B1"
+    },
+    "hérisson": {
+        article: "le",
+        level: "A2"
+    },
+    "handball": {
+        article: "le",
+        level: "A1"
+    },
+    "hâle": {
+        article: "le",
+        level: "B2"
+    },
+    "huit": {
+        article: "le",
+        level: "A1"
+    },
+    "héroïsme": {
+        article: "l'",
+        level: "B2"
+    },
+    "hasard": {
+        article: "l'",
+        level: "A2"
+    },
+    "haschich": {
+        article: "l'",
+        level: "C1"
+    },
+    "hémorragie": {
+        article: "l'",
+        level: "B2"
+    },
+    "héroïne": {
+        article: "l'",
+        level: "B1"
+    },
+    "honnêteté": {
+        article: "l'",
+        level: "B1"
+    },
+    "humilité": {
+        article: "l'",
+        level: "B2"
+    },
+    "hot-dog": {
+        article: "le",
+        level: "A1"
+    },
+    "hors-d’oeuvre": {
+        article: "le",
+        level: "B1"
+    },
+    "humidité": {
+        article: "l'",
+        level: "A2"
+    },
+    "hockeyeur": {
+        article: "le",
+        level: "A2"
+    },
+    "halogène": {
+        article: "l'",
+        level: "B2"
+    },
+    "hélicoptère": {
+        article: "l'",
+        level: "A2"
+    },
+    "hêtre": {
+        article: "le",
+        level: "B2"
+    },
+    "hennissement": {
+        article: "le",
+        level: "C1"
+    },
+    "hachoir": {
+        article: "le",
+        level: "B1"
+    },
+    "huître": {
+        article: "l'",
+        level: "A2"
+    },
+    "haricot": {
+        article: "le",
+        level: "A1"
+    },
+    "hélice": {
+        article: "l'",
+        level: "B1"
+    },
+    "housse": {
+        article: "la",
+        level: "A2"
+    },
+    "hôpital": {
+        article: "l'",
+        level: "A1"
+    },
+    "horloge": {
+        article: "l'",
+        level: "A1"
+    },
+    "habitation": {
+        article: "l'",
+        level: "A2"
+    },
+    "histoire": {
+        article: "l'",
+        level: "A1"
+    },
+    "harmonica": {
+        article: "l'",
+        level: "A2"
+    },
+    "homard": {
+        article: "le",
+        level: "B1"
+    },
+    "habitant": {
+        article: "l'",
+        level: "A2"
+    },
+    "hirondelle": {
+        article: "l'",
+        level: "B1"
+    },
+    "héritage": {
+        article: "l'",
+        level: "B1"
+    },
+    "honte": {
+        article: "la",
+        level: "A2"
+    },
+    "hystérie": {
+        article: "l'",
+        level: "B2"
+    },
+    "horizon": {
+        article: "l'",
+        level: "B1"
+    },
+    "hallucination": {
+        article: "l'",
+        level: "B2"
+    },
+    "hiérarchie": {
+        article: "la",
+        level: "B2"
+    },
+    "homme": {
+        article: "l'",
+        level: "A1"
+    },
+    "hypocrisie": {
+        article: "l'",
+        level: "B2"
+    },
+    "herbe": {
+        article: "l'",
+        level: "A1"
+    },
+    "hauteur": {
+        article: "la",
+        level: "A2"
+    },
+    "hâte": {
+        article: "la",
+        level: "A2"
+    },
+    "hommage": {
+        article: "l'",
+        level: "B1"
+    },
+    "horreur": {
+        article: "l'",
+        level: "A2"
+    },
+    "héritier": {
+        article: "l'",
+        level: "B1"
+    },
+    "honneur": {
+        article: "l'",
+        level: "B1"
+    },
+    "hygiène": {
+        article: "l'",
+        level: "A2"
+    },
+    "halte": {
+        article: "la",
+        level: "B1"
+    },
+    "hôtel": {
+        article: "l'",
+        level: "A1"
+    },
+    "humiliation": {
+        article: "l'",
+        level: "B2"
+    },
+    "humain": {
+        article: "l'",
+        level: "A2"
+    },
+    "hémisphère": {
+        article: "l'",
+        level: "B1"
+    },
+    "habilleuse": {
         article: "l'",
         level: "B2"
     }
