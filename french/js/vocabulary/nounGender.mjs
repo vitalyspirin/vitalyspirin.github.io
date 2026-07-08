914590fa5b59fbeb22f4e6c97813ec9f3af96f80
Vitaly Spirin <vitaly.spirin@gmail.com>
2026-07-08 19:45:45 -0400

    exported data (nouns) from html to nounsGender.mjs

--- french/js/vocabulary/nounGender.mjs
+++ french/js/vocabulary/nounGender.mjs
@@ -12,5 +12,321 @@ export default {
     "casquette": {
         article: "une",
         level: "A2"
+    },
+    "couteau": {
+        article: "un",
+        level: "A2"
+    },
+    "cheval": {
+        article: "un",
+        level: "A2"
+    },
+    "poire": {
+        article: "une",
+        level: "A2"
+    },
+    "couloir": {
+        article: "un",
+        level: "A2"
+    },
+    "peur": {
+        article: "une",
+        level: "A2"
+    },
+    "leçon": {
+        article: "une",
+        level: "A2"
+    },
+    "pluie": {
+        article: "une",
+        level: "A2"
+    },
+    "eau": {
+        article: "une",
+        level: "A2"
+    },
+    "paille": {
+        article: "une",
+        level: "A2"
+    },
+    "restaurant": {
+        article: "un",
+        level: "A2"
+    },
+    "chandail": {
+        article: "un",
+        level: "A2"
+    },
+    "fillette": {
+        article: "une",
+        level: "A2"
+    },
+    "été": {
+        article: "un",
+        level: "A2"
+    },
+    "musée": {
+        article: "un",
+        level: "A2"
+    },
+    "portable": {
+        article: "un",
+        level: "A2"
+    },
+    "devoir": {
+        article: "un",
+        level: "A2"
+    },
+    "exercice": {
+        article: "un",
+        level: "A2"
+    },
+    "chapeau": {
+        article: "un",
+        level: "A2"
+    },
+    "glaçon": {
+        article: "un",
+        level: "A2"
+    },
+    "princesse": {
+        article: "une",
+        level: "A2"
+    },
+    "page": {
+        article: "une",
+        level: "A2"
+    },
+    "parapluie": {
+        article: "un",
+        level: "A2"
+    },
+    "garage": {
+        article: "un",
+        level: "A2"
+    },
+    "copain": {
+        article: "un",
+        level: "A2"
+    },
+    "rideau": {
+        article: "un",
+        level: "A2"
+    },
+    "plage": {
+        article: "une",
+        level: "A2"
+    },
+    "année": {
+        article: "une",
+        level: "A2"
+    },
+    "poupée": {
+        article: "une",
+        level: "A2"
+    },
+    "fleur": {
+        article: "une",
+        level: "A2"
+    },
+    "cartable": {
+        article: "un",
+        level: "A2"
+    },
+    "peau": {
+        article: "une",
+        level: "A2"
+    },
+    "sucrier": {
+        article: "un",
+        level: "A2"
+    },
+    "sable": {
+        article: "un",
+        level: "A2"
+    },
+    "cage": {
+        article: "une",
+        level: "A2"
+    },
+    "dizaine": {
+        article: "une",
+        level: "A2"
+    },
+    "épice": {
+        article: "une",
+        level: "A2"
+    },
+    "karaté": {
+        article: "un",
+        level: "A2"
+    },
+    "érable": {
+        article: "un",
+        level: "A2"
+    },
+    "carnaval": {
+        article: "un",
+        level: "A2"
+    },
+    "calendrier": {
+        article: "un",
+        level: "A2"
+    },
+    "haut-parleur": {
+        article: "un",
+        level: "B2"
+    },
+    "promenade": {
+        article: "une",
+        level: "B2"
+    },
+    "laboratoire": {
+        article: "un",
+        level: "B2"
+    },
+    "cendrier": {
+        article: "un",
+        level: "B2"
+    },
+    "pneu": {
+        article: "un",
+        level: "B2"
+    },
+    "préparation": {
+        article: "une",
+        level: "B2"
+    },
+    "célébration": {
+        article: "une",
+        level: "B2"
+    },
+    "jardinet": {
+        article: "un",
+        level: "B2"
+    },
+    "parade": {
+        article: "une",
+        level: "B2"
+    },
+    "présence": {
+        article: "une",
+        level: "B2"
+    },
+    "confiance": {
+        article: "une",
+        level: "B2"
+    },
+    "câble": {
+        article: "un",
+        level: "B2"
+    },
+    "crochet": {
+        article: "un",
+        level: "B2"
+    },
+    "rage": {
+        article: "une",
+        level: "B2"
+    },
+    "flamme": {
+        article: "une",
+        level: "B2"
+    },
+    "fable": {
+        article: "une",
+        level: "B2"
+    },
+    "messe": {
+        article: "une",
+        level: "B2"
+    },
+    "jugement": {
+        article: "un",
+        level: "B2"
+    },
+    "conscience": {
+        article: "une",
+        level: "B2"
+    },
+    "soupçon": {
+        article: "un",
+        level: "B2"
+    },
+    "police": {
+        article: "une",
+        level: "B2"
+    },
+    "squelette": {
+        article: "un",
+        level: "B2"
+    },
+    "parenté": {
+        article: "une",
+        level: "B2"
+    },
+    "domaine": {
+        article: "un",
+        level: "B2"
+    },
+    "naufrage": {
+        article: "un",
+        level: "B2"
+    },
+    "prédiction": {
+        article: "une",
+        level: "B2"
+    },
+    "justice": {
+        article: "une",
+        level: "B2"
+    },
+    "colisée": {
+        article: "un",
+        level: "B2"
+    },
+    "sueur": {
+        article: "une",
+        level: "B2"
+    },
+    "pauvreté": {
+        article: "une",
+        level: "B2"
+    },
+    "paresse": {
+        article: "une",
+        level: "B2"
+    },
+    "capitalisme": {
+        article: "un",
+        level: "B2"
+    },
+    "gamme": {
+        article: "une",
+        level: "B2"
+    },
+    "condition": {
+        article: "une",
+        level: "B2"
+    },
+    "diagramme": {
+        article: "un",
+        level: "B2"
+    },
+    "silence": {
+        article: "un",
+        level: "B2"
+    },
+    "statuette": {
+        article: "une",
+        level: "B2"
+    },
+    "affaire": {
+        article: "une",
+        level: "B2"
+    },
+    "parlement": {
+        article: "un",
+        level: "B2"
     }
 }
