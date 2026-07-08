9d4afbd6a76ce594dc88d0ed121966bf44f56748
Vitaly Spirin <vitaly.spirin@gmail.com>
2026-07-08 19:38:46 -0400

    added empty file nounGender.mjs

---
+++ french/js/vocabulary/nounGender.mjs
@@ -0,0 +1,16 @@
+// Author: Vitaly Spirin
+//
+// word list is taken from 
+// https://oraprdnt.uqtr.uquebec.ca/portail/docs/GSC2213/F603134851_le_genre_des_noms_communs_tableau_et_exercices.pdf
+//
+
+"use strict";
+
+export const audioFileFolder = 'vocabulary/noungender';
+
+export default {
+    "casquette": {
+        article: "une",
+        level: "A2"
+    }
+}
